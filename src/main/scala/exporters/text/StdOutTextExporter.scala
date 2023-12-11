28536cccb99b66c5b7380be437903dd0ca5ebc41
Aleš Sršeň <srsenal1@fit.cvut.cz>
2023-12-11 16:16:22 +0100

    fix: Do not close System.out

--- src/main/scala/exporters/text/StdOutTextExporter.scala
+++ src/main/scala/exporters/text/StdOutTextExporter.scala
@@ -5,4 +5,13 @@ import java.io.OutputStream
 /**
  * Exports text to standard output.
  */
-class StdOutTextExporter(stdOutOutputStream: OutputStream = System.out) extends StreamTextExporter(stdOutOutputStream) {}
+class StdOutTextExporter(stdOutOutputStream: OutputStream = System.out)
+    extends StreamTextExporter(stdOutOutputStream) {
+
+  /**
+   * Do not close standard output.
+   * Closing it could cause problems for other parts of the application.
+   */
+  override def close(): Unit =
+    return
+}
